2a3c633d7b1905872ab2b25127af93dcc00c6f89
Tihersarc <Tihersarc35@gmail.com>
2023-10-11 20:00:56 +0200

    update
    TODO: enemybehaviour

---
+++ BulletHell/Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    [SerializeField] private GameObject shootPoint;
+
+    private float rotation = 0f;
+    private float timer = 0f;
+
+    enum Spawn { A, B }
+
+    [Header("Bullet")]
+    [SerializeField] private ;// -----TODO
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
