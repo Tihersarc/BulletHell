1c07d81000369859d17ff1e00b7cf92b562ecff6
Tihersarc <Tihersarc35@gmail.com>
2023-10-18 23:40:47 +0200

    Small Tweaks + Build

---
+++ BulletHell/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] soundList;
+
+    private static AudioManager Instance;
+    private static List<AudioClip> audioList;
+
+    //[SerializeField]
+    //private Slider volumeSlider;
+
+    public void Start()
+    {
+        Instance = this;
+        audioList = new List<AudioClip>();
+
+        for (int i = 0; i < soundList.Length; i++) {
+            audioList.Add(soundList[i]);
+        }
+
+    }
+    public static void PlaySound(int soundNumber)
+    {
+        Instance.gameObject.GetComponent<AudioSource>().PlayOneShot(audioList[soundNumber]);
+    }
+
+    //public void Slider()
+    //{
+    //    float volume = volumeSlider.value;
+    //    AudioListener.volume = volume;
+    //}
+}
