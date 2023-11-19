751b6f1a3dfc6eec2ee614caddaea3961cf19e6a
Tihersarc <Tihersarc35@gmail.com>
2023-11-19 12:21:47 +0100

    Added Camera Shake

--- BulletHell/Assets/Scripts/DamageBehaviour.cs
+++ BulletHell/Assets/Scripts/DamageBehaviour.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using UnityEngine;
 
 public class DamageBehaviour : MonoBehaviour
@@ -5,12 +6,24 @@ public class DamageBehaviour : MonoBehaviour
     [SerializeField]
     private int damage;
 
+    private CinemachineImpulseSource impulseSource;
+
+    private void Start()
+    {
+        impulseSource = GetComponent<CinemachineImpulseSource>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.TryGetComponent(out HealthBehaviour h) && h.enabled)
         {
             Debug.Log("hit");
             h.Damage(damage);
+
+            if (collision.gameObject.TryGetComponent(out PlayerBehaviour p))
+            {
+                CameraShakeController.instance.CameraShake(impulseSource);
+            }
         }
     }
 }
